e504fc02f60ced8968794c978004ed145cac21bb
Dvornikov Gregorii <gringaraj@gmail.com>
2023-05-03 12:25:52 +0300

    update code

--- hcsr04.py
+++ hcsr04.py
@@ -1,22 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-def getdistance():
+TRIG_PIN = 11
+ECHO_PIN = 12
 
-    #        
-    GPIO.output(11,GPIO.HIGH)
-    # Keep 10US or more
+def getdistance():    
+    GPIO.output(TRIG_PIN,GPIO.HIGH)
     time.sleep(0.000015)
-    GPIO.output(11,GPIO.LOW)
-    while not GPIO.input(12):
+    GPIO.output(TRIG_PIN,GPIO.LOW)
+    while not GPIO.input(ECHO_PIN):
         pass
-    #          
+       
     t1 = time.time()
-    while GPIO.input(12):
+    while GPIO.input(ECHO_PIN):
         pass
-    # High level end stop counting
+
     t2 = time.time()
-    #, Unit is rice
     return (t2-t1) * 340 / 2
 
 def loop():
@@ -29,9 +28,9 @@ if __name__ == "__main__":
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         # 11 Needle, GPIO17
-        GPIO.setup(11,GPIO.OUT,initial=GPIO.LOW)
+        GPIO.setup(TRIG_PIN,GPIO.OUT,initial=GPIO.LOW)
         # 12 Needle, GPIO18
-        GPIO.setup(12,GPIO.IN)
+        GPIO.setup(ECHO_PIN,GPIO.IN)
         time.sleep(1)
         loop()
     except KeyboardInterrupt:
